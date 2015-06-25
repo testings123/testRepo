9c12dc8dc10dbfae12741d66fc5fc12da35a647d
testings123 <isakki.thangam@hindujatech.com>
2015-06-25 17:52:04 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-javascript:void($("head").append("<script type=\"text/javascript\" language=\"JavaScript\" src=\"http://url.js\"></script>\""))
+$(document).ready(function(){
+  alert("asda");
+  
+});
